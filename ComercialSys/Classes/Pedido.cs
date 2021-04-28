797e24643e354efcbf82ecadd768da9f83c72e88
Wellington Santos <wellington.vsantos@gmail.com>
2021-04-28 13:47:05 -0300

    Adicionar arquivos de projeto.

---
+++ ComercialSys/Classes/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComercialSys.Classes
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public Cliente Cliente { get; set; }
+        public Usuario Usuario { get; set; }
+        public double Desconto { get; set; }
+        public string Situacao { get; set; }
+        public List<Item> Items { get; set; }
+
+    }
+}
